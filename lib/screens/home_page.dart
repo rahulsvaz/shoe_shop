acabc6f74ebacb87edd7bac43ce7abb707fd65f3
rahul <mrsvaz@icloud.com>
2023-09-14 13:58:13 +0530

    font added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override 
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-
-      
+    return Scaffold(
+      appBar: AppBar(),
       body: Column(children: [
-
-
-       
+        Center(
+          child: Text('Hello',
+              style:
+                  TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.bold)),
+        )
       ]),
     );
   }
 }
-
-
